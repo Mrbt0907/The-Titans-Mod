94712950d02e7df7ffcc91724823a62c01894866
Mrbt0907 <Mrbt0907@MrBTPC1>
2024-05-20 18:47:44 -0700

    Merge branch 'main' of https://github.com/Mrbt0907/The-Titans-Mod.git into main

--- src/main/java/net/minecraft/titans/TheTitans.java
+++ src/main/java/net/minecraft/titans/TheTitans.java
@@ -47,7 +47,7 @@ public class TheTitans
 {
 	public static final String MODNAME = "The Titans Mod";
 	public static final String MODID = "thetitans";
-	public static final String VERSION = "0.61-indev";
+	public static final String VERSION = "0.6.1-indev";
 	public static final String CLIENT = "net.minecraft.titans.ClientProxy";
 	public static final String SERVER = "net.minecraft.titans.CommonProxy";
 	@SidedProxy(clientSide=CLIENT, serverSide=SERVER)
